607d400e87f942f6937bf437349056e774ac6eba
cece95 <cesaregri@gmail.com>
2018-01-06 19:09:43 +0100

    Network and model base class structure

---
+++ gym_chess/classes/Model.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+
+class Model(nn.Module):
+    def __init__(self, architecture, activation_function):
+        super(Model, self).__init__()
+        #TODO Here we need to create each different model
+
+    def forward(self, x):
+        #TODO define forward function for the model
+        return 0
