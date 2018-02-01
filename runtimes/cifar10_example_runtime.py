4f42e08323a061b066311eddbff1cd791feeaa3c
Unknown <o3254374@rtrtr.com>
2018-01-31 22:22:38 -0800

    Fixed model params filepath

--- runtimes/cifar10_example_runtime.py
+++ runtimes/cifar10_example_runtime.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":  # Required to allow multiprocessing on windows
     testloader = torch.utils.data.DataLoader(testset, batch_size=96,
                                              shuffle=False, num_workers=4)
 
-    network = model.cifar10_model()
+    network = model.cifar10_model(filepath=save_model_path)
 
     # Train the model
     num_epochs = 10
