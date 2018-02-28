046afdc3e6e45f22a49804b6858c95398064e1ee
Unknown <o3254374@rtrtr.com>
2018-02-27 16:21:01 -0800

    Created test chess network

--- models/feed_forward.py
+++ models/feed_forward.py
@@ -20,3 +20,28 @@ class BasicConvNet(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.softmax(self.fc2(x), dim=1)
         return x
+
+
+class TestChessNet(nn.Module):
+    '''
+    literally just copied basicconvnet and
+    changed the number of inputs.
+    You can't play chess with dropout lol
+    '''
+
+    def __init__(self):
+        super(TestChessNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(64 * 6 * 6, 256)
+        self.fc2 = nn.Linear(256, 1)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = x.view(-1, 64 * 6 * 6)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
