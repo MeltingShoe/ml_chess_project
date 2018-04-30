ffb017f8d2420df6c24f3ea0b15acd338c80357c
Unknown <o3254374@rtrtr.com>
2018-04-30 03:32:38 -0700

    Improved data generation speed

--- runtimes/test.py
+++ runtimes/test.py
@@ -1,5 +1,13 @@
 import sys
-# add itself for relative imports (TODO: this should be `pip install`(ed) but the project itself may need a few `__init__` definitions to make it into a python module)
+import os
+import platform
+import torch.multiprocessing as mp
+from torch.multiprocessing import Pool
+from classes import utils
+from models import model_defs
+import time
+
+# add itself for relative imports (TODO: this should be `pip install`(ed)
 do_modify = True
 # <-- change this if your top level directory is not named ml_chess_project
 proj_path = '../ml_chess_project'
@@ -10,16 +18,6 @@ for path in sys.path:
 if do_modify:
     sys.path.insert(0, proj_path)
 
-import os
-import platform
-import torch.multiprocessing as mp
-from torch.multiprocessing import Pool
-
-from classes import utils
-from models import model_defs
-
-
-#net = model_defs.fc_12_slice
 net = model_defs.simple_cnn
 n_epochs = 40
 discount_factor = 0.5
@@ -47,32 +45,29 @@ if __name__ == '__main__':
 
     utils.ensure_dir(utils.SAVE_DIR)
 
-    def async(n_threads):
+    def async(n_episodes, n_threads):
         stack = []
         with Pool(processes=n_threads) as pool:
             results = [pool.apply_async(pack_episode)
-                       for _ in range(n_threads)]
+                       for _ in range(n_episodes)]
             for res in results:
                 stack.append(res.get())
             return stack
 
-    def async_generate_data(n_episodes, n_threads=os.cpu_count() if os.cpu_count() else 4):
-        while(n_episodes > 0):
-            if(n_threads > n_episodes):
-                n_threads = n_episodes
-            data = async(n_threads)
-            rewards_stack = []
-            states_stack = []
-            c = 0
-            metrics = {'wins': 0, 'moves': 0}
-            for _ in range(n_threads):
-                c += 1
-                a = data.pop()
-                rewards_stack += a['rewards']
-                states_stack += a['states']
-                metrics['wins'] += a['metrics']['wins']
-                metrics['moves'] += a['metrics']['moves']
-            n_episodes -= n_threads
+    def async_generate_data(n_episodes,
+                            n_threads=os.cpu_count() if os.cpu_count() else 4):
+        data = async(n_episodes, n_threads)
+        rewards_stack = []
+        states_stack = []
+        c = 0
+        metrics = {'wins': 0, 'moves': 0}
+        for _ in range(n_threads):
+            c += 1
+            a = data.pop()
+            rewards_stack += a['rewards']
+            states_stack += a['states']
+            metrics['wins'] += a['metrics']['wins']
+            metrics['moves'] += a['metrics']['moves']
 
         dataloader = utils.create_dataloader(states_stack, rewards_stack)
         return dataloader, metrics
@@ -84,8 +79,10 @@ if __name__ == '__main__':
     num_moves = 0
     run = net(resume=True)
     for i in range(100000):
-        data, metrics = async_generate_data(10, n_threads=5)
-        #data, metrics = utils.generate_data(run, 5, discount_factor)
+        start = time.time()
+        data, metrics = async_generate_data(20, n_threads=5)
+        print('time', time.time() - start)
+        # data, metrics = utils.generate_data(run, 5, discount_factor)
         num_moves += metrics['moves']
         num_wins += metrics['wins']
         num_games += 10
